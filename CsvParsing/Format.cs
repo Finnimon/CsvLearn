963f194172c0b9496d9610b4e39a563ee041b36b
Finnimon <finnimon01@gmail.com>
2025-03-14 01:57:27 +0100

    Updated to NET8 and c-sharp 12

--- CsvParsing/Format.cs
+++ CsvParsing/Format.cs
@@ -1,17 +1,7 @@
 ﻿namespace Csv;
 
-public readonly struct Format
-{
-    public Format(bool hasHeader = false, char separator = ',', string lineBreak = "\r\n", char regexEscape = '"')
-    {
-        RegexEscape = regexEscape;
-        Separator = separator;
-        LineBreak = lineBreak;
-        HasHeader = hasHeader;
-    }
-
-    public char RegexEscape { get; }
-    public char Separator { get; }
-    public string LineBreak { get; }
-    public bool HasHeader { get; }
-}
+public readonly record struct Format(
+    bool HasHeader = false,
+    char Separator = ',',
+    string LineBreak = "\r\n",
+    char RegexEscape = '"');
